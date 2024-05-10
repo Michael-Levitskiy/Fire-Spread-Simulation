23d7ffb667baccefa82153bac0356f64899b9851
Michael Levitskiy <mlevitskiy6@gmail.com>
2024-05-10 17:24:12 -0400

    Initial Code Push

---
+++ World_Tests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class World_Tests {
+    
+    // reference variables too compare
+    World testWorld, other;
+
+    @Test
+    public void testWorldSize() throws Exception{
+        testWorld = new World(15, .45);
+        other = new World(15, .45);
+
+        // check the number of rows and columns are the same
+        assertTrue(testWorld.getNumColumns() == other.getNumColumns());
+        assertTrue(testWorld.getNumRows() == other.getNumRows());
+
+        // change size for testWorld
+        testWorld = new World(10, .45);
+
+        // check the number of rows and columns are not the same
+        assertFalse(testWorld.getNumColumns() == other.getNumColumns());
+        assertFalse(testWorld.getNumRows() == other.getNumRows());
+    }
+}
